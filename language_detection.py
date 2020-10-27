85e88557f1f15483c696538d5f5b5f4e14d89de9
Rohan Rajpal <rohan17089@iiitd.ac.in>
2020-10-27 08:36:34 +0530

    More options for language detection

--- language_detection.py
+++ language_detection.py
@@ -1,5 +1,16 @@
+# google trans
 from googletrans import Translator
 translator = Translator()
 text = 'enter the language you want to detect'
 curlang=translator.detect(text)
 print(curlang)
+
+# quickest lang detect library
+import cld3
+print(cld3.get_language(text))
+
+# best accuracy with indic languages
+import langid
+print(langid.classify(text))
+
+# reference: https://stackoverflow.com/a/57349132
